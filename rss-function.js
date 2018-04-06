c5c482ff01165fa935e970fb2c36dfa65c13bd37
Dima Kostrub <freelook@mail.ua>
2018-04-06 17:02:01 +0200

    Updating rss-function.js

--- rss-function.js
+++ rss-function.js
@@ -9,19 +9,19 @@ module.exports = function(context, cb) {
   if(context.secrets.token !== context.query.token) {
     return cb('No token.');
   }
-  if(context.query.rss) {
-    const rss = context.query.rss;
-    return request(rss)
-        .pipe(new feedparser())
-        .pipe(es.writeArray(function (err, arr) {
-            if(err) {
-              return cb(err);
-            }
-            return cb(null, {rss: arr.slice(0, 100)});
-        }))
-        .on('error', function(err) {
-            cb(err);
-        });
-  } 
-  return cb('No rss param provided.');
+  if(!context.query.rss) { 
+      return cb('No rss param provided.');
+  }
+  const rss = context.query.rss;
+  return request(rss)
+      .pipe(new feedparser())
+      .pipe(es.writeArray(function (err, arr) {
+          if(err) {
+            return cb(err);
+          }
+          return cb(null, {rss: arr.slice(0, 100)});
+      }))
+      .on('error', function(err) {
+          cb(err);
+      });
 };
