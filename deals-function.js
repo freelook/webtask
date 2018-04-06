06c7b60f59f52fa2d1cf5320726cf436f2b725e0
Dima Kostrub <freelook@mail.ua>
2018-04-06 14:01:13 +0200

    Updating deals-function.js

--- deals-function.js
+++ deals-function.js
@@ -1,9 +1,16 @@
 const request = require('request');
 const es = require('event-stream');
+const RSS_FUNCTION = '';
 
 /**
 * @param context {WebtaskContext}
 */
 module.exports = function(context, cb) {
-  cb(null, { hello: context.query.name || 'Anonymous' });
+  if(context.secrets.token !== context.query.token) {
+    return cb('No token.');
+  }
+  
+  request();
+  
+  return cb(null, { hello: context.query.name || 'Anonymous' });
 };
