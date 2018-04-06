1ff4e79cc4c549f01d67a342fed83ce69d59646e
Dima Kostrub <freelook@mail.ua>
2018-04-06 16:48:18 +0200

    Updating deals-function.js

--- deals-function.js
+++ deals-function.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const es = require('event-stream');
+const as = require('async');
 
 /**
 * @param context {WebtaskContext}
@@ -8,16 +9,21 @@ module.exports = function(context, cb) {
   if(context.secrets.token !== context.query.token) {
     return cb('No token.');
   }
- return request.get({
-    url: context.secrets.rssFunction,
-    qs: {
-      token: context.secrets.token,
-      rss: context.storage.goldbox
-    }
-  }, (err, res, body) => {
-    if(err) {
-      return cb(err);
-    }
-    return cb(null, body);
-  });
+ return as.waterfall([
+   (next) => context.storage.get(next),
+   (storage, next) => {
+     request.get({
+        url: context.secrets.rssFunction,
+        qs: {
+          token: context.secrets.token,
+          rss: context.storage.goldbox
+        }
+      }, (err, res, body) => {
+        if(err) {
+          return next(err);
+        }
+        return next(null, body);
+      });
+     }
+   ], cb);
 };
