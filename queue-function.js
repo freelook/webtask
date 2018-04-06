e730346e89f567531bd40089e7c3377830b2a34b
Dima Kostrub <freelook@mail.ua>
2018-04-06 17:12:22 +0200

    Updating queue-function.js

--- queue-function.js
+++ queue-function.js
@@ -1,7 +1,7 @@
-let express = require('express');
-let wt = require('webtask-tools');
-let bodyParser = require('body-parser');
-let app = express();
+const express = require('express');
+const wt = require('webtask-tools');
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(bodyParser.json());
 
