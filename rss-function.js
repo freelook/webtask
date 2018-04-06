e73c7278e67698ba0f7b3c65b4c1e68b4a47a394
Dima Kostrub <freelook@mail.ua>
2018-04-06 13:19:45 +0200

    Updating rss-function.js

--- rss-function.js
+++ rss-function.js
@@ -6,8 +6,11 @@ const es = require('event-stream');
 * @param context {WebtaskContext}
 */
 module.exports = function(context, cb) {
+  if(context.secrets.token !== context.query.token) {
+    return cb("No token.");
+  }
   if(context.query.rss) {
-    return cb(null, { hello: context.query.rss });
+    return cb(null, { rss: context.query.rss });
   } 
   return cb("No rss param provided.");
 };
