efd1dba9e945a29aea9e298fff28b979d6a69ce8
Dima Kostrub <freelook@mail.ua>
2018-04-06 17:15:42 +0200

    Updating queue-function.js

--- queue-function.js
+++ queue-function.js
@@ -5,8 +5,12 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.sendStatus(200);
+app.get('/push', function (req, res) {
+  res.sendStatus(200, 'push');
+});
+
+app.get('/pop', function (req, res) {
+  res.sendStatus(200, 'pop');
 });
 
 module.exports = wt.fromExpress(app);
