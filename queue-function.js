b81a0fc86b38c76924bf737b2ff01cccb2603904
Dima Kostrub <freelook@mail.ua>
2018-04-07 11:10:16 +0200

    Updating queue-function.js

--- queue-function.js
+++ queue-function.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use((req, res, next) => {
   if(req.webtaskContext.secrets.token !== req.query.token) {
+     res.status(400).send('No token.');
      return next('No token.');
   }
   return next();
