c1ebbab5b4fcfa63a7ca2bd7b439e0c17d3de08b
Dima Kostrub <freelook@mail.ua>
2018-04-09 15:49:56 +0200

    Updating store-function.js

--- store-function.js
+++ store-function.js
@@ -1,6 +1,60 @@
-/**
-* @param context {WebtaskContext}
-*/
-module.exports = function(context, cb) {
-  cb(null, { hello: context.query.name || 'Anonymous' });
+const express = require('express');
+const wt = require('webtask-tools');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const as = require('async');
+const app = express();
+const router = express.Router();
+const validateMiddleware = (req, res, next) => {
+  if(req.webtaskContext.secrets.token !== req.query.token) {
+     const errMsgToken = 'No token.';
+     res.status(400).send(errMsgToken);
+     return next(errMsgToken);
+  }
+  return next();
 };
+const StoreSchema = mongoose.Schema({
+  updated: { 
+    type: Date,
+    default: Date.now
+  }, 
+  payload: Schema.Types.Mixed
+});
+const mongoDbMiddleware = (req, res, next) => {
+  mongoose.connect(req.webtaskContext.secrets.mongo, (err) => {
+    req.Store = mongoose.model('Store', StoreSchema);
+    next(err);
+  });
+};
+const responseHandler = (err, res, data) => {
+  if(!!err) {
+    return res.status(400).json(err);
+  }
+  return res.status(200).json(data);
+};
+
+router
+.get('/:id', function (req, res) {
+  as.waterfall([
+
+  ],
+  (err, data) => responseHandler(err, res, data));
+})
+.post('/:id?', function (req, res) {
+  as.waterfall([
+
+  ],
+  (err, data)=> responseHandler(err, res, data));
+})
+.patch('/:id', function (req, res) {
+  as.waterfall([
+    
+  ],
+  (err, data)=> responseHandler(err, res, data));
+});
+
+app
+.use(bodyParser.json())
+.use('/', validateMiddleware, mongoDbMiddleware, router);
+
+module.exports = wt.fromExpress(app);
