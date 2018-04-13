ae7b697d8630e829df04dd78c2674cb483ebcf41
Dima Kostrub <freelook@mail.ua>
2018-04-13 10:44:08 +0200

    Create publisher-function.js

---
+++ publisher-function.js
@@ -0,0 +1,30 @@
+const request = require('request');
+const as = require('async');
+
+const loader = (params, next) => {
+  request.get({
+    url: params.url,
+    qs: params.qs
+  }, (err, res, body) => {
+    if(!!err || res.statusCode !== 200 || !body) {
+      return next(err || body || 'No body.');
+    }
+    const msg = JSON.parse(body);
+    return next(null, msg);
+  });
+};
+
+/**
+* @param context {WebtaskContext}
+*/
+module.exports = function(context, cb) {
+  if(context.secrets.token !== context.query.token) {
+    return cb('No token.');
+  }
+  return as.waterfall([
+   (next) => context.storage.get(next),
+   (storage, next) => {
+     next();
+   },
+  ], cb);
+};
