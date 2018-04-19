95a50c37c63a7fe4aa243ced280b094667419423
Dima Kostrub <freelook@mail.ua>
2018-04-19 17:02:34 +0200

    Updating notification-function.js

--- notification-function.js
+++ notification-function.js
@@ -12,7 +12,6 @@ const loader = (params, next) => {
     if(!!err || res.statusCode !== 200 || !body) {
       return next(err || body || 'No body.');
     }
-    //const msg = JSON.parse(body);
     return next(null, body);
   });
 };
