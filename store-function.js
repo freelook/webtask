6dbec449acd688c17aa9aea9a509c97f220fa532
Dima Kostrub <freelook@mail.ua>
2018-04-19 16:03:55 +0200

    Updating store-function.js

--- store-function.js
+++ store-function.js
@@ -36,7 +36,7 @@ StoreSchema.pre('save', function (next) {
   next();
 });
 StoreSchema.post('save', function(item, next) {
-  console.log('1', item.state); 
+  console.log('1', item.state);
   if(!!this.isStreamRequired) {
     console.log('2');
     //streamer(req.webtaskContext)(item, ()=>{});
