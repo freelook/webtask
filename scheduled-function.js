6103a26fe55aa535ab36fc1774289d35b22f2849
Dima Kostrub <freelook@mail.ua>
2018-04-20 09:45:31 +0200

    Updating scheduled-function.js

--- scheduled-function.js
+++ scheduled-function.js
@@ -28,8 +28,8 @@ module.exports = function(context, cb) {
       qs: {token: context.secrets.token}
     }, next),
     (msg, next) => {
-      console.log('scheduled', msg);
-      if(msg && msg.payload) {
+      if(msg && msg.payload) {      
+        console.log('scheduled', msg.payload);
         return loader({
           method: 'put',
           url: `${context.secrets.storeFunction}/${msg.payload}`,
