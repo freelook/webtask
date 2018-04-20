ab70d53890447f84ed7ed76f0286553e96a243ea
Dima Kostrub <freelook@mail.ua>
2018-04-20 13:31:27 +0200

    Updating scheduler-function.js

--- scheduler-function.js
+++ scheduler-function.js
@@ -44,8 +44,6 @@ const ddHandler = (context) => (storage, next) => {
 * @param context {WebtaskContext}
 */
 module.exports = function(context, cb) {
-  console.log(_.get(context, 'body.container'));
-  console.log(context.secrets.token);
   if(context.secrets.token !== _.get(context, 'body.container')) {
     return cb('No token.');
   }
