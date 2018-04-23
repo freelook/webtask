815857d700d0007167aca69fc231fd0a0d97a6af
Dima Kostrub <freelook@mail.ua>
2018-04-23 17:01:00 +0200

    Updating phantom-function.js

--- phantom-function.js
+++ phantom-function.js
@@ -23,6 +23,7 @@ const waitFor = (page, selector) => {
 * @param context {WebtaskContext}
 */
 module.exports = function(context, req, res) {
+  console.log(req);
   var url = req.url.split('/phantom-function/')[1] || context.query.url;
   if(!url) {
     return res.end('No url provided.');
