9c9051e92c322ccbbff4e3b00b19af8602ca88bd
Dima Kostrub <freelook@mail.ua>
2018-04-25 09:59:08 +0200

    Updating scheduled-function.js

--- scheduled-function.js
+++ scheduled-function.js
@@ -1,22 +1,7 @@
 const request = require('request');
 const as = require('async');
 const _ = require('lodash');
-
-const loader = (params, next) => {
-  request({
-    method: (params.method || 'get').toUpperCase(),
-    url: params.url,
-    qs: params.qs,
-    json: params.json
-  }, (err, res, body) => {
-    if(!!err || res.statusCode !== 200 || !body) {
-      return next(err || body || 'No body.');
-    }
-    var msg = body;
-    try {msg = typeof body === 'string' ? JSON.parse(body) : body;} catch(e) {}
-    return next(null, msg);
-  });
-};
+const loader = require('fli-webtask').lib.loader;
 
 const ack = (context) => (params, next) => loader({
     url: `${context.secrets.queueFunction}/ack/${params.msg.ack}`,
