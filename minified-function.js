ef72b8606d6eb90687a2c5dd004b4453e0dd2f80
Dima Kostrub <freelook@mail.ua>
2018-05-15 16:03:19 +0200

    Create minified-function.js

---
+++ minified-function.js
@@ -0,0 +1,43 @@
+const fli = require('fli-webtask');
+const request = fli.npm.request;
+const as = fli.npm.async;
+const _ = fli.npm.lodash;
+const loader = fli.lib.loader;
+
+/**
+* @param context {WebtaskContext}
+*/
+module.exports = function(context, cb) {
+  if(context.secrets.token !== context.query.token) {
+    return cb('No token.');
+  }
+  if(!_.get(context, 'body._id')) {
+    return cb('No _id provided.');
+  }
+  if(!_.get(context, 'body.payload.asin')) {
+    return cb('No asin provided.');
+  }
+  return as.waterfall([
+   (next) => loader({
+      url: context.secrets.amazonFunction,
+      qs: {
+        token: context.secrets.token,
+        url: context.body.payload.url
+      }
+    }, next),
+    (info, next) => loader({
+      method: 'patch',
+      url: `${context.secrets.storeFunction}/${context.body._id}`,
+      qs: {token: context.secrets.token},
+      json: {info: info}
+    }, () => next(null, info)),
+    (info, next) => loader({
+      method: 'put',
+      url: `${context.secrets.storeFunction}/${context.body._id}`,
+      qs: {token: context.secrets.token},
+      json: {
+        state: 'informed'
+      }
+    }, () => next(null, info))
+  ], cb);
+};
