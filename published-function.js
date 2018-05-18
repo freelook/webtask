511e3e933ba61a10053f0f23e54fdfd49ebaaa2f
Dima Kostrub <freelook@mail.ua>
2018-05-18 14:36:23 +0200

    Create published-function.js

---
+++ published-function.js
@@ -0,0 +1,33 @@
+const fli = require('fli-webtask');
+const request = fli.npm.request;
+const as = fli.npm.async;
+const _ = fli.npm.lodash;
+const loader = fli.lib.loader;
+
+/**
+* @param context {WebtaskContext}
+*/
+module.exports = function(context, cb) {
+  if(context.secrets.token !== context.query.token) {
+    return cb('No token.');
+  }
+  if(!_.get(context, 'body._id')) {
+    return cb('No _id provided.');
+  }
+  return as.waterfall([
+   (next) => loader({
+      method: 'post',
+      url: context.secrets.publisherFunction,
+      qs: {token: context.secrets.token},
+      json: context.body,
+    }, () => next()),
+    (info, next) => loader({
+      method: 'put',
+      url: `${context.secrets.storeFunction}/${context.body._id}`,
+      qs: {token: context.secrets.token},
+      json: {
+        state: 'published'
+      }
+    }, () => next(null, info))
+  ], cb);
+};
