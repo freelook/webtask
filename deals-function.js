9a3ed32a9f0e423fbfcb06dbcc5e542358f2ab42
Dima Kostrub <freelook@mail.ua>
2018-05-24 16:14:35 +0200

    Updating deals-function.js

--- deals-function.js
+++ deals-function.js
@@ -29,7 +29,8 @@ module.exports = function(context, cb) {
       const description = _.get(deal, 'description', '');
       next(null, {
        promoText: _.get(deal, 'title', ''),
-       promoImg: (description.match(/.+img src="(.+?)".+/) || [])[1] || '',
+       promoImg: ((description.match(/.+img src="(.+?)".+/) || [])[1] || '')
+                 .replace('._SL160_.', '._SL1000_.'),
        asin: (link.match(/.+\/dp\/([\w]+)\/.+/) || [])[1] || '',
        node: (link.match(/.+node=([\w]+)&.+/) || [])[1] || '',
        url: link.replace(context.secrets.rssTag, context.secrets.fliTag)
