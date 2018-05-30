24410a172ee0eb16420cb0dc8870c8e52a8c3b3c
Dima Kostrub <freelook@mail.ua>
2018-05-30 09:29:10 +0200

    Updating minified-function.js

--- minified-function.js
+++ minified-function.js
@@ -26,7 +26,7 @@ module.exports = function(context, cb) {
       url: context.secrets.amazonFunction,
       qs: {
         token: context.secrets.token,
-        url: encodeURIComponent(context.body.payload.url)
+        url: context.body.payload.url
       }
     }, next),
     (info, next) => loader({
