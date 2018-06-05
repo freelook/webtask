575b7c3a468b2906ae2dda76184841bec4c6dd4c
Dima Kostrub <freelook@mail.ua>
2018-06-05 16:51:59 +0200

    Updating amazon-function.js

--- amazon-function.js
+++ amazon-function.js
@@ -94,7 +94,7 @@ router
       }
       req.oph.execute('BrowseNodeLookup', {
         'BrowseNodeId': node,
-        'ResponseGroup': 'BrowseNodeInfo'
+        'ResponseGroup': 'NewReleases'
       })
       .then((info) => {
         next(null, {
