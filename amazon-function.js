0c2ee32c1f385e460e6d2b514fda41772b6fea07
Dima Kostrub <freelook@mail.ua>
2018-06-06 15:38:28 +0200

    Updating amazon-function.js

--- amazon-function.js
+++ amazon-function.js
@@ -38,6 +38,7 @@ const responseHandler = (err, res, data) => {
 };
 const jsonMapper = (asin) => (info , next) => {
   var item = _.get(info, 'result.ItemLookupResponse.Items.Item');
+  return next(null, 'info');
   if(!item || item.ASIN !== asin) {
     return next('Incorrect info.');
   }
