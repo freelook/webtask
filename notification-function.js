f77caac5cfc13a99ea0ddad59ad95e0154e84710
Dima Kostrub <freelook@mail.ua>
2018-06-06 13:46:51 +0200

    Updating notification-function.js

--- notification-function.js
+++ notification-function.js
@@ -1,19 +1,6 @@
-const request = require('request');
-const as = require('async');
-
-const loader = (params, next) => {
-  request({
-    method: (params.method || 'get').toUpperCase(),
-    url: params.url,
-    qs: params.qs,
-    json: params.json
-  }, (err, res, body) => {
-    if(!!err || res.statusCode !== 200 || !body) {
-      return next(err || body || 'No body.');
-    }
-    return next(null, body);
-  });
-};
+const fli = require('fli-webtask');
+const as = fli.npm.async;
+const loader = fli.lib.loader;
 
 const notifier = (context) => (params, next) => as.map(
   params.sources,
