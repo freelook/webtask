e61e9914f83f601bc9f42ef4610deaebf1f8d021
Dima Kostrub <freelook@mail.ua>
2018-06-10 16:56:17 +0200

    Updating deals-function.js

--- deals-function.js
+++ deals-function.js
@@ -10,16 +10,13 @@ module.exports = function(context, cb) {
   if(context.secrets.token !== context.query.token) {
     return cb('No token.');
   }
-  if(!context.query.endpoint) {
-    return cb('No endpoint param provided.');
-  }
   return as.waterfall([
    (next) => context.storage.get(next),
    (storage, next) => loader({
       url: context.secrets.rssFunction,
       qs: {
         token: context.secrets.token,
-        rss: storage[context.query.endpoint]
+        rss: storage.endpoint
       }
    }, next),
    (data, next) => as.mapSeries(_.get(data, 'rss', []),
