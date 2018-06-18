f326b40fb07acdc531dccafe64c80b7c7722cae6
Dima Kostrub <freelook@mail.ua>
2018-06-18 16:32:09 +0200

    Updating scheduler-function.js

--- scheduler-function.js
+++ scheduler-function.js
@@ -38,7 +38,6 @@ const cronHandler = (context) => (storage, next) => {
 * @param context {WebtaskContext}
 */
 module.exports = function(context, cb) {
-  console.log(_.get(context, 'body.container'));
   if(context.secrets.container !== _.get(context, 'body.container')) {
     return cb('No container token.');
   }
