62fe5832e4ee5c1c8bb09ce80d276f462961c798
Dima Kostrub <freelook@mail.ua>
2018-06-22 10:51:07 +0200

    Updating store-function.js

--- store-function.js
+++ store-function.js
@@ -16,7 +16,7 @@ const streamer = (req) => (item, next) => loader({
     url: req.webtaskContext.secrets.notificationFunction,
     qs: {token: req.webtaskContext.secrets.token, topic: item.state},
     json: (() => {
-      var json = Object.assign({db:req.params.db}, item);
+      var json = Object.assign({db:req.params.db}, item.toObject());
       console.log(json);
       return json;
     })()
