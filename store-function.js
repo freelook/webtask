6a00b563cfd2a9f2c80059b803d38e186291f23d
Dima Kostrub <freelook@mail.ua>
2018-06-29 18:27:44 +0200

    Updating store-function.js

--- store-function.js
+++ store-function.js
@@ -82,7 +82,7 @@ router
 })
 .get('/query/updated/:updated', function (req, res) {
   as.waterfall([
-    (next) => req.Store.find({updated: {$date: /2018-06-29/mi}}, next)
+    (next) => req.Store.find({updated: {$regex: /2018-06-29/mi}}, next)
   ],
   (err, data) => responseHandler(err, res, data));
 })
