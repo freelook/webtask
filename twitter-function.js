2ef2672a0b1627f1d680ee0ee4375f0a6f73ea9b
freelook.info <freelook@mail.ua>
2018-09-14 15:20:27 +0200

    Updating twitter-function.js

--- twitter-function.js
+++ twitter-function.js
@@ -55,7 +55,7 @@ router
   const url = req.twUrl;
   const imgUrl = _.get(req, 'body.payload.promoImg') || _.get(req, 'body.payload.info.image');
   const promoText = _.get(req, 'body.payload.promoText') || _.get(req, 'body.payload.info.title');
-  const hashTags = [''].concat(_.get(req, 'body.payload.info.labels', []).map(h => h.replace(/[^\w\d]/mig, ''))).concat(['Today', 'Amazon', 'Deal']).join(' #');
+  const hashTags = [''].concat(_.get(req, 'body.payload.info.labels', []).map(h => h.replace(/[^\w\d]/mig, ''))).concat(['Amazon', 'Deal']).join(' #');
   console.log(`-- twitter published: ${promoText} ${url}`);
   as.waterfall([
    (next) => fli.npm.request({
