009ef2ac5c9d508468427c70740479aa0c1abb3c
freelook.info <freelook@mail.ua>
2019-04-04 13:30:50 +0200

    Updating facebook-function.js

--- facebook-function.js
+++ facebook-function.js
@@ -82,7 +82,7 @@ router
     _.get(req, 'body.payload.info.labels', [])
     .map(h => h.replace(/[^\w\d]/mig, ''))
     .filter(h => h && h.length < 33)
-    ).concat(['Amazon', 'Deal']).join(' %23');
+    ).concat(['Amazon', 'Deal']).join(' #');
   console.log(`-- facebook published: ${promoText} ${url}`);
   as.waterfall([
    (next) => getToken(req, next),
