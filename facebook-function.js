2fdf6a89174f461b9328d6aa5be346676ce7dcfc
freelook.info <freelook@mail.ua>
2019-10-28 09:55:18 +0100

    Updating facebook-function.js

--- facebook-function.js
+++ facebook-function.js
@@ -74,7 +74,7 @@ const getToken = (req, cb) => {
         url: `${context.secrets.alarmFunction}/${req.db}/record`,
         qs: {
           token: context.secrets.token,
-          name: 'facebook_publish_error'
+          name: 'facebook_publish_error' 
         },
       }, () => {});
       return cb(err);
