919bac833154c23ce3d4925f979aa5df81981db5
freelook.info <freelook@mail.ua>
2020-01-15 15:01:52 +0100

    Updating rss-function.js

--- rss-function.js
+++ rss-function.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const request = require('request');
 const feedparser = require('feedparser');
 const es = require('event-stream');
@@ -9,17 +10,18 @@ module.exports = function(context, cb) {
   if(context.secrets.token !== context.query.token) {
     return cb('No token.');
   }
-  if(!context.query.rss) { 
+  const rss = _.get(context, 'query.rss', _.get(context, 'body.rss')); 
+  if(!rss) { 
       return cb('No rss param provided.');
   }
-  const rss = context.query.rss;
+  const max = _.get(context, 'query.max', _.get(context, 'body.max', _.get(context, 'secrets.max')));
   return request(rss)
       .pipe(new feedparser())
       .pipe(es.writeArray(function (err, arr) {
           if(err) {
             return cb(err);
           }
-          return cb(null, {rss: arr.slice(0, +context.secrets.max)});
+          return cb(null, {rss: arr.slice(0, +max)});
       }))
       .on('error', function(err) {
           cb(err);
