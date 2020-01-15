7531c5e033c2b0a580b31bc1f6fdde23d5dc7c2c
freelook.info <freelook@mail.ua>
2020-01-15 17:00:28 +0100

    Updating rss-function.js

--- rss-function.js
+++ rss-function.js
@@ -11,7 +11,6 @@ module.exports = function(context, cb) {
     return cb('No token.');
   }
   const rss = _.get(context, 'query.rss', _.get(context, 'body.rss'));
-  console.log(context);
   if(!rss) {
       return cb('No rss param provided.');
   }
