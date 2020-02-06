d798f823c7673322063ee083e8735b981b94ba8c
freelook.info <freelook@mail.ua>
2020-02-06 15:51:27 +0100

    Create meta-function.js

---
+++ meta-function.js
@@ -0,0 +1,24 @@
+const _ = require('lodash');
+const util = require('util');
+const metaget = require('metaget');
+
+/**
+* @param context {WebtaskContext}
+*/
+module.exports = async function(context, cb) {
+  if(context.secrets.token !== context.query.token) {
+    return cb('No token.');
+  }
+  const url = _.get(context, 'query.url', _.get(context, 'body.url'));
+  if(!url) {
+      return cb('No url param provided.');
+  }
+  let meta = {};
+  try {
+    meta = await util.promisify(metaget.fetch)(url);
+  } catch(e) {
+    meta = { error: _.toString(e) };
+  } finally {
+    cb(null, meta);
+  }
+};
