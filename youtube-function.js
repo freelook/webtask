531c7324a47338f57d1f2bddc7df9436eec28a40
freelook.info <freelook@mail.ua>
2020-02-15 17:42:47 +0100

    Updating youtube-function.js

--- youtube-function.js
+++ youtube-function.js
@@ -213,10 +213,11 @@ router
           }), 'data' );
         }));
         store.id.length = Math.min(store.id.length, 10);
-        console.log(store); 
+        console.log(store);
         await util.promisify((data, next) => req.webtaskContext.storage.set(data, next))(store);
         return {data: {comments, videos}};
       } catch(err) {
+        console.log(err);
         return err;
       }
     }
