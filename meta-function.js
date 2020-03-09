4d9908fd8bf878d6a73e6d0ffce2c9f90063e766
freelook.info <freelook@mail.ua>
2020-03-09 14:33:29 +0100

    Updating meta-function.js

--- meta-function.js
+++ meta-function.js
@@ -1,6 +1,68 @@
 const _ = require('lodash');
 const util = require('util');
-const metaget = require('metaget');
+const cheerio = require('cheerio');
+const request = require('request');
+
+const metaget = function (uri, user_options, callback) {
+        var options = {
+            url: uri,
+            timeout: 5000,
+            headers: {
+                'User-Agent': 'request'
+            }
+        };
+
+        //  setup the args/user_options
+        var user_args = [];
+        for (var i = 0; i < arguments.length; i++) {
+            user_args.push(arguments[i]);
+        }
+
+        // remove these from arg array
+        uri = user_args.shift();
+        callback = user_args.pop();
+
+        // get user_options if specified
+        if (user_args.length > 0) {
+            user_options = user_args.shift();
+        } else {
+            user_options = null;
+        }
+
+        // override default headers
+        if (user_options) {
+            options.headers = user_options.headers;
+        }
+
+        request.get(options, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                var $ = cheerio.load(body);
+                var meta = $('meta');
+                var keys = Object.keys(meta);
+                var meta_obj = {};
+                keys.forEach(function (key) {
+                    if (meta[key].attribs != undefined) {
+                        if (meta[key].attribs.property && meta[key].attribs.content) {
+                            meta_obj[meta[key].attribs.property] = meta[key].attribs.content;
+                        }
+                        if (meta[key].attribs.name && meta[key].attribs.content) {
+                            meta_obj[meta[key].attribs.name] = meta[key].attribs.content;
+                        }
+                    }
+                });
+                
+                mata_obj['title'] = $('title').text();
+                
+                callback(null, meta_obj);
+            } else {
+                if (response && typeof response.statusCode !== 'undefined') {
+                    callback('Response code: ' + response.statusCode, null);
+                } else {
+                    callback(error, null);
+                }
+            }
+        });
+};
 
 /**
 * @param context {WebtaskContext}
@@ -15,7 +77,7 @@ module.exports = async function(context, cb) {
   }
   let meta = {};
   try {
-    meta = await util.promisify(metaget.fetch)(
+    meta = await util.promisify(metaget)(
       url,
       {
         headers: {
