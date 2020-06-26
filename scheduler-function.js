7884a2f7341357d7dde26d5b4e6b04435abc2d10
freelook.info <freelook@mail.ua>
2020-06-26 12:53:33 +0200

    Updating scheduler-function.js

--- scheduler-function.js
+++ scheduler-function.js
@@ -10,7 +10,7 @@ const worker = (context) => (params, cb) => as.map(
   (task, next) => {
     loader({
     url: context.secrets[task],
-    qs: {token: context.secrets.token, alarm: true}
+    qs: {token: context.secrets.token, alarm: false}
   }, () => {});
   next();
   },
