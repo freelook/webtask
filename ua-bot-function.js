80289b3c68efee2064e5325770b01a18da43a8d6
freelook.info <freelook@mail.ua>
2020-11-25 12:35:16 +0100

    Updating ua-bot-function.js

--- ua-bot-function.js
+++ ua-bot-function.js
@@ -16,11 +16,11 @@ router
   const feed = req.body;
   console.log(feed, req.query);
   if(_.includes(feed, req.webtaskContext.secrets.topic)) {
-    const entry = (feed.match(/<entry>(.*)<\/entry>/mi) || [])[1] || '';
-    const videoId = (entry.match(/<yt:videoId>(.*)<\/yt:videoId>/mi) || [])[1];
-    const channelId = (entry.match(/<yt:channelId>(.*)<\/yt:channelId>/mi) || [])[1];
-    const published = (entry.match(/<published>(.*)<\/published>/mi) || [])[1];
-    const title = (entry.match(/<title>(.*)<\/title>/mi) || [])[1];
+    const entry = ((/<entry>(.*)<\/entry>/mi).exec(feed) || [])[0] || '';
+    const videoId = (entry.match(/<yt:videoId>(.*)<\/yt:videoId>/) || [])[1];
+    const channelId = (entry.match(/<yt:channelId>(.*)<\/yt:channelId>/) || [])[1];
+    const published = (entry.match(/<published>(.*)<\/published>/) || [])[1];
+    const title = (entry.match(/<title>(.*)<\/title>/) || [])[1];
     const channelName = (entry.match(/<name>(.*)<\/name>/) || [])[1];
     console.log({videoId, channelId, published, title, channelName});
     if(videoId && channelId && published && title && channelName) {
