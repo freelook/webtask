49a43f1166cbca9c6424bdb78de6033b5cea708d
freelook.info <freelook@mail.ua>
2020-11-26 11:48:38 +0100

    Updating youtube-function.js

--- youtube-function.js
+++ youtube-function.js
@@ -236,7 +236,7 @@ router
       let id = req.params.id;
       return like({auth, id, context: req.webtaskContext}, next);
     }
-  ], (err, likeResult) => responseHandler(err, res, _.get(likeResult, 'data', {})));
+  ], (err, likeResult) => responseHandler(err, res, _.get(likeResult, 'data', likeResult)));
 })
 .all('/comment', function (req, res) {
   console.log(`-- google youtube comment`);
