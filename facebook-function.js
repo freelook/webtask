815116f7d0ffe6b4d8c72b08a3a1e1ed0b8f99e5
freelook.info <freelook@mail.ua>
2021-01-03 15:04:01 +0100

    Updating facebook-function.js

--- facebook-function.js
+++ facebook-function.js
@@ -89,6 +89,10 @@ const getToken = (req, cb) => {
 router
 .all('/publish', function (req, res) {
   const url = req.fUrl;
+  const promoType = _.get(req, 'body.payload.promoType');
+  if(promoType !== 'PROMOCODE') {
+    return responseHandler('Skip deal', res);
+  }
   const promoText = _.get(req, 'body.payload.promoText') || _.get(req, 'body.payload.info.title');
   const imgUrl = _.get(req, 'body.payload.promoImg') || _.get(req, 'body.payload.info.image');
   const hashTags = [''].concat(
