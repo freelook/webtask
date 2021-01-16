e787a1581b2aea0bc36323e7a80d4c0d99c81171
freelook.info <freelook@mail.ua>
2021-01-16 19:23:16 +0100

    Updating ua-bot-function.js

--- ua-bot-function.js
+++ ua-bot-function.js
@@ -78,7 +78,7 @@ router
             }
           }, () => {});
           
-          store.id.length = Math.min(store.id.length, 250);
+          store.id.length = Math.min(store.id.length, 200);
           await util.promisify((data, next) => req.webtaskContext.storage.set(data, next))(store);
         }
       }
