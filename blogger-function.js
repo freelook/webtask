4f42974d3fddd2c6722e144c73454101f3cab537
freelook.info <freelook@mail.ua>
2021-02-25 13:46:53 +0100

    Updating blogger-function.js

--- blogger-function.js
+++ blogger-function.js
@@ -114,7 +114,7 @@ router
     }
   ],
   (err, response) => {
-    responseHandler(null, res, err || _.get(response, 'data', {}));
+    responseHandler(null, res, _.get(response, 'data', {}));
   });
 });
 
